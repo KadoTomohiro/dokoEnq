6af7f3315d655b497dcb6a549b7d128c38272fa4
KadoTomohiro <kado.tomohiro@jp.fujitsu.com>
2016-01-14 13:53:14 +0900

    プロジェクト生成

---
+++ app/scripts/main.js
@@ -0,0 +1,36 @@
+/**
+ * This is dokoEnq module.
+ *
+ * @module dokoEnq
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('dokoEnq', [
+      'ngNewRouter',
+      'dokoEnq.config',
+      'dokoEnq.components.home',
+      'dokoEnq.components.about',
+      'dokoEnq.components.contact'
+    ])
+    .controller('AppController', AppController);
+
+  AppController.$routeConfig = [
+    {path: '/',       redirectTo: '/home'},
+    {path: '/home',    component: 'home'},
+    {path: '/about',   component: 'about'},
+    {path: '/contact', component: 'contact'}
+  ];
+
+  AppController.$inject = [];
+
+  /**
+   * AppController
+   *
+   * @class AppController
+   * @main dokoEnq
+   * @constructor
+   */
+  function AppController () {}
+})();
