58cbfd23fde3fe8cfa99e92544a5f8b19546a031
Hiroyuki Suminoue <suminoue.hiro@jp.fujitsu.com>
2016-02-05 15:56:30 +0900

    シングルクォート

--- app/service/question/question.js
+++ app/service/question/question.js
@@ -9,46 +9,46 @@
   function questionService($log, $resource) {
     var get = function(enqueteId) {
       return {
-        title: 'セミナーについて',
-        discription: 'セミナーについてのご意見をお聞かせください',
-        open: true,
-        groups: {
-          group1: {
-            discription: '受講者情報',
-            questions: ['q1', 'q2']
+        'title': 'セミナーについて',
+        'discription': 'セミナーについてのご意見をお聞かせください',
+        'open': true,
+        'groups': {
+          'group1': {
+            'discription': '受講者情報',
+            'questions': ['q1', 'q2']
           },
-          group2: {
-            discription: 'セミナーについて',
-            questions: ['q3', 'q4']
+          'group2': {
+            'discription': 'セミナーについて',
+            'questions': ['q3', 'q4']
           }
         },
-        questions: {
-          q1: {
-            question: '氏名',
-            type: 'text',
-            format: 'text',
-            require: true
+        'questions': {
+          'q1': {
+            'question': '氏名',
+            'type': 'text',
+            'format': 'text',
+            'require': true
           },
-          q2: {
-            question: '所属',
-            type: 'select',
-            format: 'single',
-            items: ['JA', 'アグリ', 'モバイル', 'IDC', '営業', '金融', 'Sビジ'],
-            require: true
+          'q2': {
+            'question': '所属',
+            'type': 'select',
+            'format': 'single',
+            'items': ['JA', 'アグリ', 'モバイル', 'IDC', '営業', '金融', 'Sビジ'],
+            'require': true
           },
-          q3: {
-            question: 'セミナーに参加した目的を教えてください',
-            type: 'select',
-            format: 'mulch',
-            items: ['業務活用のため', 'お客様提案のため', '学習のため'],
-            require: true
+          'q3': {
+            'question': 'セミナーに参加した目的を教えてください',
+            'type': 'select',
+            'format': 'mulch',
+            'items': ['業務活用のため', 'お客様提案のため', '学習のため'],
+            'require': true
           },
-          q4: {
-            question: 'セミナーに参加した目的は達成できましたか',
-            type: 'select',
-            format: 'mulch',
-            items: ['達成できた', '概ね達成できた', 'あまり達成できなかった', '達成できなかった'],
-            require: true
+          'q4': {
+            'question': 'セミナーに参加した目的は達成できましたか',
+            'type': 'select',
+            'format': 'mulch',
+            'items': ['達成できた', '概ね達成できた', 'あまり達成できなかった', '達成できなかった'],
+            'require': true
           }
         }
       };
